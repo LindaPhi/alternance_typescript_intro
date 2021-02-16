02f5781d71f35540190044ef70395e393d4935e7
LindaPhi <linda.phinouvong@le-campus-numerique.fr>
2021-02-16 11:17:26 +0100

    Step 6

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,32 @@
 
-console.log("Hello ");
+const prompts = require('prompts');
+// @ts-ignore
+import Character from "./Character";
+(async () => {
+    const question =  [
+        {
+            type: 'text',
+            name: 'name',
+            message: 'Veuillez renseigner votre nom de personnage :'
+        },
+        {
+            type: 'select',
+            name: 'gender',
+            message: 'Veuillez renseigner le genre de votre personnage F/M/N :',
+            choices: [
+                { title: 'Female', value: 'Female' },
+                { title: 'Male', value: 'Male' },
+                { title: 'No-gender', value: 'No-gender' }
+            ]
+        }
+
+    ];
+
+    const response = await prompts(question);
+    let myCharacter: Character =  new Character(response.name,response.gender,100);
+    myCharacter.summary();
+    console.log('Ennemi en approche !');
+    })();
+
+
+
